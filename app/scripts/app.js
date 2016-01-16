b4563749f0b38e734fb9a6d975008a428cbe9fd7
jsudufour <jsudufour@gmail.com>
2016-01-15 20:58:01 -0500

    Initial app setup.

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+(function() {
+	function config($stateProvider, $locationProvider) {
+		$locationProvider
+			.html5Mode({
+				enabled: true,
+				requireBase: false
+		});
+
+		$stateProvider
+			.state('active', {
+				url: '/active',
+				controller: 'ActiveCtrl as active',
+				templateUrl: '/templates/active.html'
+			})
+
+			.state('inactive', {
+				url: '/inactive',
+				controller: 'InactiveCtrl as inactive',
+				templateUrl: '/templates/inactive.html'
+			});
+		}
+
+	angular
+		.module('blocJams', ['ui.router', 'firebase'])
+		.config(config);
+
+})();
